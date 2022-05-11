166d2405c028a5e81a57e1ecbf87ad15e30b24c3
Jamie <jamielintaipei@gmail.com>
2022-05-11 14:47:11 +0800

    2-1 建立 Server 並監聽變化（ 內建模組 http）

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 //第一區塊 內建模組
-const path = require('path');
 const http = require('http');
 
 // 第二個區塊 第三方模組(套件)
-const cowsay = require('cowsay');
 //第三區塊 自建模組
 //require 是匯入hello.js檔案
-const hello = require('./hello');
+
 
 /////////////////////////////////////////////////////////////
 
-//使用say的方法跟module，從hello.js
-console.log(cowsay.say({
-    text : "I'm a moooodule",
-    e : "@@",
-    T : "U "
-}));
+const server = http.createServer((req, res) => {
+	// console.log('第一個參數是瀏覽器對 web server 的 request', req);
+	// console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
+    console.log(req.url);
+    if (req.url === '/login'){
+        return res.end('This is login page');
+    }
+	res.end();
+});
 
+server.listen(3000, () => {
+	console.log('running server on port 3000');
+});
