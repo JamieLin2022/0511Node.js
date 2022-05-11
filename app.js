fe51fdc5232ba27349a5901d6f526f3ca4966878
Jamie <jamielintaipei@gmail.com>
2022-05-11 11:34:53 +0800

    補充: coding style

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+//第一區塊 內建模組
+const path = reuire('path');
+const http = require('http');
+
+// 第二個區塊 第三方模組(套件)
+
+//第三區塊 自建模組
 //require 是匯入hello.js檔案
 const hello = require('./hello');
 
+/////////////////////////////////////////////////////////////
 
-//使用say的方法跟module
+//使用say的方法跟module，從hello.js
 hello.sayHello();
 
 console.log(hello.title);
