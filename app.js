0af27183b56474080ffc221a62711ea96a416b69
Jamie <jamielintaipei@gmail.com>
2022-05-11 13:37:45 +0800

    1-5 第一次使用第三方套件就上手（第三方套件 cowsay）

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //第一區塊 內建模組
-const path = reuire('path');
+const path = require('path');
 const http = require('http');
 
 // 第二個區塊 第三方模組(套件)
-
+const cowsay = require('cowsay');
 //第三區塊 自建模組
 //require 是匯入hello.js檔案
 const hello = require('./hello');
@@ -11,6 +11,9 @@ const hello = require('./hello');
 /////////////////////////////////////////////////////////////
 
 //使用say的方法跟module，從hello.js
-hello.sayHello();
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
 
-console.log(hello.title);
