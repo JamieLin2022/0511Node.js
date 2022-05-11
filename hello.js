8b63b14454c83eebe35eae28d3fd0b4626a76e24
Jamie <jamielintaipei@gmail.com>
2022-05-11 11:30:49 +0800

    1-2-2 使用 path module

--- hello.js
+++ hello.js
@@ -1,4 +1,6 @@
 // hello.js
+const path = require('path');
+
 const title='I am Hello Module';
 
 const sayHello = () => {
@@ -6,16 +8,23 @@ const sayHello = () => {
 };
 
 //匯出上面這個module，say是自訂的匯出名稱
-module.exports.sayHello = sayHello;
+// module.exports.sayHello = sayHello;
 
 
 // module.exports.sayHello = sayHello;
 
+//如果key 跟 value是一樣的內容，可以直接省略value的內容 (ex:sayHello)
 // module.exports = {
-//     sayHello: sayHello,
+//     sayHello,
 //     sayGoodnight: sayGoodnight,
 //     title: title
 // };
+
+//動態提供當下的位置
+// console.log('dirname', __dirname);
+// console.log('filename', __filename);
+// console.log(path.join(__dirname, 'index.js'));
+
 
 // JS remarks:
 module.exports = {
@@ -23,3 +32,5 @@ module.exports = {
     sayGoodnight,
     title,
 };
+
+console.log('module', module);
