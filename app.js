19b8b98b466e8bf81923d1189c6947f9f768c9f9
Jamie <jamielintaipei@gmail.com>
2022-05-12 10:52:29 +0800

    3-4 製作 login 頁面並處理 POST request（第三方套件 body-parser）

--- app.js
+++ app.js
@@ -1,49 +1,45 @@
-//第一區塊 內建模組
+// 第一個區塊 內建模組
 const path = require('path');
 
 // 第二個區塊 第三方模組(套件)
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-//第三區塊 自建模組
+// 第三個區塊 自建模組
 
-
-
-/////////////////////////////////////////////////////////////
-
+////////////////////////////////////////////////////////////////
 const app = express();
 
+// middleware
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-//middleware 介於瀏覽器 web server之間
-// app.use((req, res, next) => {
-// 	console.log('Hello!');
-//     next();
-// });
-
-// app.use((req, res, next) => {
-// 	console.log('World!');
-//     next();
-// });
-
-//get是一個路由的請求，在localhost:3000底下
+app.use((req, res, next) => {
+	console.log('Hello!');
+    next();
+});
+app.use((req, res, next) => {
+	console.log('World!');
+    next();
+});
 app.get('/', (req, res) => {
-    //node js 內建模組做法
-    // res.writeHead(200, { 'Content-Type': 'text/html' });
-    // res.write('<head><meta charset="utf-8" /></head>')
-    // res.write('<body>')
-    // res.write('<h1>這是首頁</h1>')
-    // res.write('</body>')
-    //express套建模組做法
-    res.status(200) //把網頁連接到views裡的index.html，200代表請求成功
+    res.status(200)
         .sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-
 app.get('/login', (req, res) => {
-    res.status(200) 
-    .sendFile(path.join(__dirname, 'views', 'login.html'));
+    res.status(200)
+        .sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    if (email && password) {
+        res.redirect('/');
+    } else {
+        console.log('欄位尚未填寫完成！')
+    }
 });
 
 app.listen(3000, () => {
 	console.log('Web Server is running on port 3000');
-});
+}); 
