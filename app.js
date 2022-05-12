fb7539aac0bc4db32960d56a16dfd008281bc82f
Jamie <jamielintaipei@gmail.com>
2022-05-12 09:19:55 +0800

    3-2.b 製作 login page

--- app.js
+++ app.js
@@ -25,13 +25,20 @@ app.use((req, res, next) => {
 
 //get是一個路由的請求，在localhost:3000底下
 app.get('/', (req, res) => {
+    //node js 內建模組做法
     // res.writeHead(200, { 'Content-Type': 'text/html' });
     // res.write('<head><meta charset="utf-8" /></head>')
     // res.write('<body>')
     // res.write('<h1>這是首頁</h1>')
     // res.write('</body>')
+    //express套建模組做法
     res.status(200) //把網頁連接到views裡的index.html
         .sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.status(200) 
+    .sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
 app.listen(3000, () => {
